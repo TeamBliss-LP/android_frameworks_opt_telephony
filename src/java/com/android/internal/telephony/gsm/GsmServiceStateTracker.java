lt03lte-cafaosp/platform-frameworks-opt-telephony

Check for DDS validity
The default subId for a phone without the sim card is -1
but the default value of DDS when there is no sim is -1000.
As a result, even when there is no sim it takes 30 seconds to
turn on APM. Fix this by checking for DDS to be valid

CRs-Fixed: 815010

Change-Id: Ie6abb74065b718cebe0fbe06501c48677095bee8

src/java/com/android/internal/telephony/gsm/GsmServiceStateTracker.java
@@ -2076,7 +2076,8 @@ final class GsmServiceStateTracker extends ServiceStateTracker {
                 if (dcTracker.isDisconnected()
                         && (dds == mPhone.getSubId()
                             || (dds != mPhone.getSubId()
-                                && ProxyController.getInstance().isDataDisconnected(dds)))) {
+                                && ProxyController.getInstance().isDataDisconnected(dds))
+                            || !SubscriptionManager.isValidSubId(dds))) {
                     // To minimize race conditions we do this after isDisconnected
                     dcTracker.cleanUpAllConnections(Phone.REASON_RADIO_TURNED_OFF);
                     if (DBG) log("Data disconnected, turn off radio right away.");

TeamBliss-LP/android_frameworks_opt_telephony

Change-Id: Ice7c44ebc2b70c5ac8d9e7f3e7dd01f804e4f639

src/java/com/android/internal/telephony/gsm/GsmServiceStateTracker.java
@@ -2195,7 +2195,8 @@ final class GsmServiceStateTracker extends ServiceStateTracker {
                 if (dcTracker.isDisconnected()
                         && (dds == mPhone.getSubId()
                             || (dds != mPhone.getSubId()
-                                && ProxyController.getInstance().isDataDisconnected(dds)))) {
+                                && ProxyController.getInstance().isDataDisconnected(dds))
+                            || !SubscriptionManager.isValidSubscriptionId(dds))) {
                     // To minimize race conditions we do this after isDisconnected
                     dcTracker.cleanUpAllConnections(Phone.REASON_RADIO_TURNED_OFF);
                     if (DBG) log("Data disconnected, turn off radio right away.");